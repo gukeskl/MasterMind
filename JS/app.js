0e06886886774f82711b8d8c7cb0cb8428e4a04d
Jakub Markowski <gukeskl@gmail.com>
2020-05-09 17:48:00 +0200

    Full project without settings page

--- JS/app.js
+++ JS/app.js
@@ -8,7 +8,8 @@ let game_settings = {
     difficulty_lv,
     numberOfColors: 0,
     correct_colors_table: [],
-    actual_game_choices: []
+    actual_game_choices: [],
+    round_counter: 0
 }
 
 
@@ -16,7 +17,15 @@ let game_settings = {
 start_button.addEventListener('click', function(){
 
     set_game_settings();
+
     menu_div.classList.toggle('menu__out');
+    setTimeout(() => {
+      menu_div.style.display = 'none';
+      game.style.display = 'flex';
+      game.classList.add("active");
+      game.style.opacity = '1';
+    }, 500);
+
     rand_colorCombination(game_settings.difficulty_lv , game_settings.numberOfColors);
     console.log(game_settings.correct_colors_table);
     prepare_game_panel();
@@ -24,3 +33,4 @@ start_button.addEventListener('click', function(){
     );
 
 check_button.addEventListener('click', check);
+newGame_button.addEventListener('click', () => {location.reload();});
--- JS/basicsEl_DOM_conection.js
+++ JS/basicsEl_DOM_conection.js
@@ -3,6 +3,7 @@ const menu_div = document.querySelector('.menu');
 const actual_game = document.querySelector('.actual__game');
 const game_elements = document.querySelector('.game__elements')
 const game_history = document.querySelector('.history');
+const game = document.querySelector('#game');
 
 //buttons conection
 const start_button = document.querySelector('#start__button');
